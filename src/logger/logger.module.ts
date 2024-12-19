0db3fc1acab835138c0f9ce9cdbc42cdde203d51
Konstantin Altukhov <kostuyn@gmail.com>
2024-12-19 20:12:07 +0300

    add logger

--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -1,29 +1,28 @@
 import { CustomLogger } from '@/logger/custom-logger';
 import { InitLoggerMiddleware } from './request-id.middleware';
-import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AsyncLocalStorage } from 'node:async_hooks';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { ReqResInterceptor } from '@/logger-new/req-res.interceptor';
-import { ReqResMiddleware } from 'src/logger-new/req-res.middlewar';
+import { Logger, MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { AsyncLocalStorage } from "node:async_hooks";
+import { ReqResMiddleware } from "src/logger/req-res.middlewar";
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [
-    CustomLogger,
-    {
-      provide: AsyncLocalStorage,
-      useValue: new AsyncLocalStorage<Logger>(),
-    },
-    // {
-    //     provide: APP_INTERCEPTOR,
-    //     useClass: ReqResInterceptor,
-    // }
-  ],
-  exports: [CustomLogger],
+    imports: [],
+    controllers: [],
+    providers: [
+        CustomLogger,
+        {
+            provide: AsyncLocalStorage,
+            useValue: new AsyncLocalStorage<Logger>(),
+        },
+    ],
+    exports: [CustomLogger],
 })
 export class LoggerModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(InitLoggerMiddleware, ReqResMiddleware).forRoutes('*');
-  }
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(
+                InitLoggerMiddleware,
+                ReqResMiddleware,
+            )
+            .forRoutes('*');
+    }
 }
